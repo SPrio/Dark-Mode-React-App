93c99edad352c37a0a03c5ea95a9fbae02d01172
Supriyo Mahanta <mahantasupriyo@gmail.com>
2020-11-22 15:18:53 +0530

    added basic ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Typography, Button, Paper } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper style={{ height: "100vh" }}>
+      <Grid container direction="column">
+        <Typography variant="h1">This is my App!</Typography>
+        <Button variant="contained" color="primary">
+          This is a button
+        </Button>
+        <Button variant="contained" color="secondary">
+          This is another button 
+        </Button>
+      </Grid>
+    </Paper>
   );
 }
 
