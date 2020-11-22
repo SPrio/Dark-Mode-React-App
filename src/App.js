da49f62c6834ae434bae29ca622c7d5c7c9feca6
SPrio <mahantasupriyo@gmail.com>
2020-11-22 18:45:13 +0530

    added dark theme toggler

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
-import { Grid, Typography, Button, Paper } from "@material-ui/core";
+import React, {useState} from "react";
+import {
+  Grid,
+  Typography,
+  Button,
+  Paper,
+  ThemeProvider,
+  createMuiTheme,
+  Switch,
+} from "@material-ui/core";
+import { dark } from "@material-ui/core/styles/createPalette";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <Paper style={{ height: "100vh" }}>
-      <Grid container direction="column">
-        <Typography variant="h1">This is my App!</Typography>
-        <Button variant="contained" color="primary">
-          This is a button
-        </Button>
-        <Button variant="contained" color="secondary">
-          This is another button 
-        </Button>
-      </Grid>
-    </Paper>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <Grid container direction="column">
+          <Typography variant="h1">This is my App!</Typography>
+          <Button variant="contained" color = "primary">This is a button</Button>
+          <Button variant="contained" color="secondary">
+            This is another button
+          </Button>
+          <Switch checked={darkMode} onChange={()=> setDarkMode(!darkMode)}>
+
+          </Switch>
+        </Grid>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
